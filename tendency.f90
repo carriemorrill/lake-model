f0257a2dfc6fe4bf26c6a7e65de46a62f443a297
carriemorrill <carrie.morrill@noaa.gov>
2020-07-22 09:43:03 -0600

    Add files via upload

--- tendency.f90
+++ tendency.f90
@@ -6,7 +6,9 @@
       subroutine tendency (li, idtend, ta_in, ta_i, qa_in, qa_i,        &
                           ua_in, ua_i, sw_in, sw_i, rlwd_in, rlwd_i,    &
                           ps_in, ps_i, rh_in, rh_i, prec_in, prec_i,    &
-                          runin_in, runin_i)   
+                          runin_in, runin_i, d2Hp_in, d2Hp_i,           &
+                          d18Op_in, d18Op_i, d2Hr_in, d2Hr_i,           &
+                          d18Or_in, d18Or_i)   
   
       implicit none
       include 'lake.inc'
@@ -29,6 +31,14 @@
       real prec_i     ! interpolated precipitation for this dt [m]
       real runin_in(2)! basin runoff from input [m]
       real runin_i    ! interpolated basin runoff for this dt [m]
+      real d2Hp_in(2) ! delta 2H of precipitation from input read [per mil]
+      real d2Hp_i     ! interpolated basin runoff for this dt [m]
+      real d18Op_in(2)! delta 18O of precipitation from input read [per mil]
+      real d18Op_i    ! interpolated basin runoff for this dt [m]
+      real d2Hr_in(2) ! delta 2H of runoff from input read [per mil]
+      real d2Hr_i     ! interpolated delta 2H of runoff for this dt [per mil]
+      real d18Or_in(2)! delta 18O of runoff from input read [per mil]
+      real d18Or_i    ! interpolated delta 18O of runoff for this dt [per mil]
       integer li      ! number of time steps since input read #1
       integer idtend  ! total number of time steps between input reads #1 and #2
 
@@ -44,6 +54,19 @@
       prec_i = prec_in(1) / real(idtend)  ! accumulations, not rates
       runin_i = runin_in(1) / real(idtend)
 
+      if(d2H_flag) then
+         d2Hp_i = d2Hp_in(1) + (li - 1) * &
+                 (d2Hp_in(2) - d2Hp_in(1)) / real(idtend)
+         d2Hr_i = d2Hr_in(1) + (li - 1) * &
+                 (d2Hr_in(2) - d2Hr_in(1)) / real(idtend)
+      end if
+      if(d18O_flag) then
+         d18Op_i = d18Op_in(1) + (li - 1) * &
+                 (d18Op_in(2) - d18Op_in(1)) / real(idtend)
+         d18Or_i = d18Or_in(1) + (li - 1) * &
+                 (d18Or_in(2) - d18Or_in(1)) / real(idtend)
+      end if
+
       if (li.eq.idtend) then
          ta_in(1) = ta_in(2)
          qa_in(1) = qa_in(2)
@@ -54,6 +77,10 @@
          rh_in(1) = rh_in(2)
          prec_in(1) = prec_in(2)
          runin_in(1) = runin_in(2)
+         d2Hp_in(1) = d2Hp_in(2)
+         d18Op_in(1) = d18Op_in(2)
+         d2Hr_in(1) = d2Hr_in(2)
+         d18Or_in(1) = d18Or_in(2)
       endif
  
       return
